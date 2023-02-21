7ab3bab77d58bbe51b6e0e335be76cccab489461
Serigne Ousmane Kana Ciss <kanaman6@gmail.com>
2023-02-21 17:10:54 -0600

    Update settings.py

--- inventory/inventory/settings.py
+++ inventory/inventory/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-uurejuog^hfjvwfik_&2yydst&umj)gw%bhq8#+4+1*m@e*f%8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
