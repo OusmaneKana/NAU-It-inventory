92e7515af0f3ef69b8628b014ec560623d349422
Serigne Ousmane Kana Ciss <kanaman6@gmail.com>
2023-03-06 11:42:10 -0600

    Database Update
    Update column names
    remove mongod library

--- inventory/mainDash/models.py
+++ inventory/mainDash/models.py
@@ -1,14 +1,13 @@
-# from django.db import models
-from djongo import models
+from django.db import models
 import uuid
 class Device(models.Model):
-    _id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    device_name = models.CharField(max_length= 50)
-    devie_model = models.CharField(max_length= 50)
-    device_serial_number = models.CharField(max_length= 50)
-    device_qty = models.IntegerField()
-    device_price = models.IntegerField()
-    device_sellers_origin = models.CharField(max_length= 50)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    make = models.CharField(max_length= 50)
+    model = models.CharField(max_length= 50)
+    serial_number = models.CharField(max_length= 50)
+    qty = models.IntegerField()
+    price = models.IntegerField()
+    sellers_origin = models.CharField(max_length= 50)
 
 
 class Record(models.Model):
